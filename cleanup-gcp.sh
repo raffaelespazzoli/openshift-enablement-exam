a96db61546d67e269ddb7be88afb793e191c9d3c
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-11 11:07:21 -0400

    fix

--- cleanup-gcp.sh
+++ cleanup-gcp.sh
@@ -9,21 +9,21 @@ for i in $(gcloud compute forwarding-rules list | awk 'NR>1 {print $1}'); do
 done
 
 #delete target pools
-for i in $(gcloud compute target-pools list | awk 'NR>1 {print $1}') do
+for i in $(gcloud compute target-pools list | awk 'NR>1 {print $1}'); do
 	gcloud compute target-pools delete -q $i --region "us-central1";
 done
 
 #delete static address
-for i in $(gcloud compute addresses list | awk 'NR>1 {print $1}') do
+for i in $(gcloud compute addresses list | awk 'NR>1 {print $1}'); do
 	gcloud compute addresses delete -q $i --region "us-central1";
 done
 
 #delete VMs
-for i in $(gcloud compute instances list | awk 'NR>1 {print $1}') do
+for i in $(gcloud compute instances list | awk 'NR>1 {print $1}'); do
 	gcloud compute instances delete -q $i --region "us-central1";
 done
 
 #delete disks
-for i in $(gcloud compute disks list | awk 'NR>1 {print $1}') do
+for i in $(gcloud compute disks list | awk 'NR>1 {print $1}'); do
 	gcloud compute disks delete -q $i --region "us-central1";
 done
