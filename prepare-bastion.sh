e0a3c2502f9fcd1d04178084f722f37edb4c10aa
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-11 14:59:02 -0400

    configure bastion

--- prepare-bastion.sh
+++ prepare-bastion.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 set -e
-ansible nfs -b -i hosts -m shell -a 'yum install -y subscription-manager && subscription-manager clean && subscription-manager register --username=$RHN_USERNAME --password=$RHN_PASSWORD && subscription-manager attach --pool=8a85f9843e3d687a013e3ddd471a083e && subscription-manager refresh && subscription-manager repos --disable="*" && subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-optional-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.3-rpms" && yum update -y && yum install -y git ansible atomic-openshift-utils && echo StrictHostKeyChecking no >> /etc/ssh/ssh_config'
+#install subcription manager and clean
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'sudo yum install -y subscription-manager && sudo subscription-manager clean'
+#subscribe
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` sudo subscription-manager register --username=$RHN_USERNAME --password=$RHN_PASSWORD
+# configure subscriptions
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'sudo subscription-manager attach --pool=8a85f9843e3d687a013e3ddd471a083e && sudo subscription-manager refresh && sudo subscription-manager repos --disable="*" && sudo subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-optional-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.3-rpms"'
+#update install packages
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'sudo yum update -y && sudo yum install -y git ansible atomic-openshift-utils'
 # generate and add keys
-ssh `gcloud compute instance describe ose-bastion region us-central1 | grep address: | awk '{print $2}'` ssh-keygen -t rsa -f .ssh/id_rsa -N ''
-ssh `gcloud compute instance describe ose-bastion region us-central1 | grep address: | awk '{print $2}'` cat /home/rspazzol/.ssh/id_rsa.pub | gcloud compute project-info add-metadata --metadata-from-file sshKeys=-
+ssh `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` ssh-keygen -t rsa -f .ssh/id_rsa -N ''
+# set the key in gcloud metadata
+ssh `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` cat /home/rspazzol/.ssh/id_rsa.pub | gcloud compute project-info add-metadata --metadata-from-file sshKeys=-
