c1079ad058fe22f98fc7242f6fa02787515162b9
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-20 20:53:16 -0400

    f

--- misc/reset-cluster.sh
+++ misc/reset-cluster.sh
@@ -8,6 +8,6 @@ oc adm drain master3.c.openshift-enablement-exam2.internal --ignore-daemonsets
 oc label node infranode1.c.openshift-enablement-exam2.internal region=infra zone=default --overwrite
 oc label node infranode2.c.openshift-enablement-exam2.internal region=infra zone=default --overwrite 
 
-node1.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
-node2.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
-node3.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
+oc label node node1.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
+oc label node node2.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
+oc label node node3.c.openshift-enablement-exam2.internal region=primary zone=default --overwrite
