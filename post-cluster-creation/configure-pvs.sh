b4bac62c082a077f0cd6c0409a8540cd344717c1
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-30 19:36:45 -0400

    f

--- post-cluster-creation/configure-pvs.sh
+++ post-cluster-creation/configure-pvs.sh
@@ -3,8 +3,8 @@ set -e
 
 #crate necessary dirs in nfs server
 
-ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'for i in {1..30} do echo "/exports/pv$i *(rw,root_squash)" | sudo tee -a /etc/exports.d/openshift-ansible.exports > /dev/null done'
-ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'for i in {1..30} do sudo mkdir "/exports/pv$i && sudo chown nfsnobody:nfsnobody "/exports/pv$i && sudo chmod 777 "/exports/pv$i done'
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'for i in {1..30}; do echo "/exports/pv$i *(rw,root_squash)" | sudo tee -a /etc/exports.d/openshift-ansible.exports > /dev/null done'
+ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'for i in {1..30}; do sudo mkdir "/exports/pv$i && sudo chown nfsnobody:nfsnobody "/exports/pv$i && sudo chmod 777 "/exports/pv$i done'
 
 #restart nfs
 ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` sudo systemctl restart nfs
