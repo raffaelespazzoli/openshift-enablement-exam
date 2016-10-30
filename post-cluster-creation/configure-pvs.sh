832c4eabea7872de1a90227212ba44046a43066b
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-10-30 19:23:13 -0400

    f

--- post-cluster-creation/configure-pvs.sh
+++ post-cluster-creation/configure-pvs.sh
@@ -2,7 +2,7 @@
 set -e
 
 #crate necessary dirs in nfs server
-for i {1..30}; do
+for i in {1..30} do
 	ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` 'echo "/exports/pv$i *(rw,root_squash)" | sudo tee -a /etc/exports.d/openshift-ansible.exports > /dev/null'
 	ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` sudo mkdir "/exports/pv$i"
 	ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` sudo chown nfsnobody:nfsnobody "/exports/pv$i"
@@ -13,6 +13,6 @@ done
 ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` sudo systemctl restart nfs
 
 # create pvs 
-for i {1..30}; do
+for i in {1..30} do
 	oc process -f pv_template.yaml -v NFS_EXPORT="pv$i",PV_NAME="pv$i"
 done
