3c8db5238454c978978ee3a221aa138a66f14c03
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2016-12-27 08:54:58 -0500

    removed set vars

--- setvars1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export GCLOUD_PROJECT=openshift-enablement-exam1
-export DNS_DOMAIN=gc1.raffa.systems
-export RHN_USERNAME=rhn-gps-rspazzol
-export RHN_PASSWORD=skywalker
-export SSH_PUB_KEY=$HOME/.ssh/id_rsa.pub
