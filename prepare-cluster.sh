7e570bcf79b5002451013709172789e064762592
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2017-08-16 21:59:57 -0400

    fixes

--- prepare-cluster.sh
+++ prepare-cluster.sh
@@ -6,7 +6,6 @@ ansible nodes -b -i hosts -m shell -a "yum install -y subscription-manager && su
 ansible nodes -b -i hosts -m shell -a "subscription-manager register --username=$RHN_USERNAME --password=$RHN_PASSWORD && subscription-manager attach --pool=$RHN_SUB_POOL && subscription-manager refresh" 
 ansible nodes -b -i hosts -m shell -a "subscription-manager repos --disable='*' && subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-optional-rpms --enable=rhel-7-server-extras-rpms --enable=rhel-7-server-ose-3.6-rpms --enable=rhel-7-fast-datapath-rpms"
 ansible nodes -b -i hosts -m shell -a "yum update -y && yum install -y docker wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
-ansible nodes -b -i hosts -m shell -a
 ansible 'nodes:!masters' -i hosts -b -m copy -a "src=docker-storage-setup dest=/etc/sysconfig/docker-storage-setup"
 #this is non-idempotent
 ansible 'nodes:!masters' -i hosts -b -m shell -a "yum install -y docker && docker-storage-setup"
