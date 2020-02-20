5422fc61ce55cd642b681a1874cbe27a74fd7945
Raffaele Spazzoli <raffaele.spazzoli@gmail.com>
2020-02-20 15:58:34 +0100

    added setup on azure+ipv6
    Signed-off-by: Raffaele Spazzoli <raffaele.spazzoli@gmail.com>

--- 4.0/setup.sh
+++ 4.0/setup.sh
@@ -6,9 +6,14 @@ function create_openshift()
 {
   mkdir -p ./cluster$CLUSTER_ID
   cp ./config/install-config-raffa$CLUSTER_ID.yaml ./cluster$CLUSTER_ID/install-config.yaml
-  openshift-install create cluster --dir ./cluster$CLUSTER_ID --log-level debug
+  ~/Downloads/openshift-install-linux-4.4.0-0.nightly-2020-02-17-103442/openshift-install create cluster --dir ./cluster$CLUSTER_ID --log-level debug
   export KUBECONFIG=/home/rspazzol/git/openshift-enablement-exam/4.0/cluster1/auth/kubeconfig
+  # create route 
   oc create route reencrypt apiserver --service kubernetes --port https -n default
+  # add simple user
+  htpasswd -c -B -b ./cluster$CLUSTER_ID/auth/htpasswd raffa raffa
+  oc create secret generic htpass-secret --from-file=htpasswd=./cluster$CLUSTER_ID/auth/htpasswd -n openshift-config
+  oc apply -f ../misc4.0/htpasswd/oauth.yaml -n openshift-config
 }
 
 #download_installer
